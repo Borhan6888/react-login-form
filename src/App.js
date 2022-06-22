1f1b7f07a2efab08444c8b52c06d46a5f5f5c1e8
BORHAN <borhanuddin6888@gmail.com>
2022-06-22 12:34:49 +0600

    React login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Homepage from './components/homepage/Homepage';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [user, setLoginUser] = useState({});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/">
+            {user && user._id ? (
+              <Homepage setLoginUser={setLoginUser} />
+            ) : (
+              <Login setLoginUser={setLoginUser} />
+            )}
+          </Route>
+          <Route path="login">
+            <Login setLoginUser={setLoginUser} />
+          </Route>
+          <Route path="register">
+            <Register />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
